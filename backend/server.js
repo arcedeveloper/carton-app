23bacdb6a3d6ebdb90f86816b89fb6662728d458
matirodas50-eng <matirodas50@gmail.com>
2026-03-16 23:19:04 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 300,
     message: { error: 'Demasiadas peticiones, intentá más tarde' }
 });
 app.use('/api/', limiter);
